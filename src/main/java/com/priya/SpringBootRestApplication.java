bbd51ff9f90311d6c3d69e7420d50a4193040f94
Lakshmi Priya <lakshmi.priya8806@gmail.com>
2019-01-06 12:31:29 -0500

    Changes in SprintBoot Main class to enable war deployment

--- src/main/java/com/priya/SpringBootRestApplication.java
+++ src/main/java/com/priya/SpringBootRestApplication.java
@@ -2,15 +2,25 @@ package com.priya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class SpringBootRestApplication {
+public class SpringBootRestApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestApplication.class, args);
-	}
+    @Override
+    /**
+     * Making the existing @SpringBootApplication class extends SpringBootServletInitializer
+     * to support WAR deployment.
+     */
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootRestApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootRestApplication.class, args);
+    }
 
 }
-
